2d7e16b0ef2b1d027c5152fa0553821d2202fe87
sinangoktas <sinan.goktas@gmail.com>
2017-04-28 23:33:15 +0100

    new files added

--- src/StructuringInformation/Duration.scala
+++ src/StructuringInformation/Duration.scala
@@ -10,7 +10,7 @@ case object Quarter extends Duration
 
 /*
 It is generally a bad idea to work with a model that makes it possible to reach invalid states as in Note.scala
-Note names can easily bet set to an invalid note name. Note names are in fact consists of A,B,C,D,E,F,G only.
+Note names can easily be set to an invalid note name. Note names are in fact consists of A,B,C,D,E,F,G only.
 In our case, we want to restrict the possible durations to a set of fixed alternatives. Whole, Half and Quarter...
 We can express the fact that durations are a fixed set of alternatives by using a sealed trait,
 but in contrast to the Symbol.scala, alternatives are not case classes because they aggregate no information.
