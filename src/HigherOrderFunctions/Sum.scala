6581ef50711d06f710a84ce2815357c25d06796c
sinangoktas <sinan.goktas@gmail.com>
2017-04-29 00:52:54 +0100

    new files added

--- src/HigherOrderFunctions/Sum.scala
+++ src/HigherOrderFunctions/Sum.scala
@@ -2,7 +2,10 @@ package HigherOrderFunctions
 import com.atomicscala.AtomicTest._
 
 /**
-  * Created by cyn on 29/04/17.
+Functional languages treat functions as first-class values.
+This means that, like any other value, a function can be passed as a parameter and returned as a result.
+This provides a flexible way to compose programs.
+Functions that take other functions as parameters or that return functions as results are called higher order functions.
   */
 object Sum extends App {
 
